0fc7b9ae602267994cdb81aefe81ac299da99318
dardwo <dardwo@st.amu.edu.pl>
2023-05-19 16:21:51 +0200

    all

---
+++ courseapp/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:courseapp/data_uploader_screen.dart';
+import 'package:courseapp/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  //WidgetsFlutterBinding.ensureInitialized();
+  //await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(GetMaterialApp(home:DataUploaderScreen()));
+}
+
+
